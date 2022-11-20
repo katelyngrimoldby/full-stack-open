2e8103f2d11e6ab840321b24fe4e05066d4c5e9d
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-20 13:12:45 -0700

    1.4

--- part1/course-info/src/components/Total.jsx
+++ part1/course-info/src/components/Total.jsx
@@ -1,4 +1,6 @@
-const Total = ({totalExercises}) => {
+const Total = ({parts}) => {
+  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises
+
   return <p>Number of exercises: {totalExercises}</p>
 }
 
