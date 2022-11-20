9066695d27ca59a25654109a82e4544595bedca2
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-20 13:47:41 -0700

    1.9

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -7,16 +7,21 @@ const Statistics = ({good, neutral, bad}) => {
     return (good / (good + neutral + bad)) * 100
   }
 
-  return(
-    <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>Total: {good + bad + neutral}</p>
-        <p>Average: {getAverage(good, neutral, bad)}</p>
-        <p>Positive: {getPositive(good, neutral, bad)}%</p>
-      </div>
-  )
+  if(good > 0 || neutral > 0 || bad > 0) {
+    return(
+      <div>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total: {good + bad + neutral}</p>
+          <p>Average: {getAverage(good, neutral, bad)}</p>
+          <p>Positive: {getPositive(good, neutral, bad)}%</p>
+        </div>
+    )
+  } else {
+    return <p>No feedback given</p>
+  }
+  
 }
 
 export default Statistics;
