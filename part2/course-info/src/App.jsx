c5675dce94c379b385b6aa9b917127e647026093
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-20 15:02:33 -0700

    2.4 & 2.5

--- part2/course-info/src/App.jsx
+++ part2/course-info/src/App.jsx
@@ -2,17 +2,26 @@ import Course from "./components/Course"
 
 
 function App() {
-  const course = {
+  const courses = [
+    {
     name: 'Half Stack application development',
     parts: [
       {name: 'Fundamentals of React', exercises: 10},
       {name: 'Using props to pass data', exercises: 7},
       {name: 'State of a component', exercises: 14}
     ]
-}
+  }, 
+  {
+    name: 'Node.js',
+    parts: [
+      {name: 'Routing', exercises: 3},
+      {name: 'Middlewares', exercises: 7}
+    ]
+  }
+]
   return (
     <div className="App">
-     <Course course={course} />
+     {courses.map((course, index) => <Course key={index + Date.now()} course={course} />)}
     </div>
   )
 }
