c18160f64dc75df5e252712f874f9cfaa88b19af
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-20 13:33:58 -0700

    1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,11 +2,31 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div className="App">
-      
+      <h1>Unicafe</h1>
+      <h2>Give Feedback</h2>
+      <div>
+        <button onClick={() => {
+          setGood(good + 1)
+        }}>Good</button>
+        <button onClick={() => {
+          setNeutral(neutral + 1)
+        }}>Neutral</button>
+        <button onClick={() => {
+          setBad(bad + 1)
+        }}>Bad</button>
+      </div>
+      <h2>Statistics</h2>
+      <div>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
     </div>
   )
 }
