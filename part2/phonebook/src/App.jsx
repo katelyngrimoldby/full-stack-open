bfab11950445d63b2b8184b9473d2348c405eb1e
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-20 15:18:39 -0700

    2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,31 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [entries, setEntries] = useState([])
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const copy = entries.concat({name: newName})
+    setEntries(copy)
+
+    setNewName('')
+  }
 
   return (
     <div className="App">
-      
+      <h1>Phonebook</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nameInput">Name: </label>
+        <input id="nameInput" value={newName} onChange={(event) => setNewName(event.target.value)} />
+        <button type="submit">Add</button>
+      </form>
+
+      <h2>Numbers</h2>
+      <div>
+        {entries.map(entry => <p key={entry.name}>{entry.name}</p>)}
+      </div>
     </div>
   )
 }
