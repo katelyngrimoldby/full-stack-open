66139c982d187430f172cc7e18f27e5f31846b8b
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-26 16:41:06 -0700

    5.16

--- part4/bloglist/src/tests/Blog.test.jsx
+++ part4/bloglist/src/tests/Blog.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
 import Blog from '../components/Blog';
 
 describe('Blog component', () => {
