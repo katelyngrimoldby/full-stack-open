b0f1606f217ef78ad88d3be0b1bd63e00657aabc
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-26 13:06:02 -0700

    5.7

--- part4/bloglist/src/components/Blog.jsx
+++ part4/bloglist/src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const handleToggle = () => {
+    setVisible(!visible);
+  };
+
+  return(
+    <div>
+      <p>
+        {blog.title} {blog.author}{' '}
+        <button onClick={handleToggle}>{visible ? 'hide' : 'show'}</button>
+      </p>
+      {visible && (
+        <div>
+          <p>{blog.url}</p>
+          <p>Likes: {blog.likes} <button>Like</button></p>
+          <p>Added by: {blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
