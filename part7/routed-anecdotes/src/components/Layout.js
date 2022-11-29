45e510a4d735618e37aa6538445fb63aaa9c3133
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-28 19:32:20 -0700

    7.1

---
+++ part7/routed-anecdotes/src/components/Layout.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  const padding = {
+    paddingRight: 5
+  }
+
+  return (
+    <>
+      <div>
+        <Link to='/anecdotes' style={padding}>anecdotes</Link>
+        <Link to='/create-new' style={padding}>create new</Link>
+        <Link to='/about' style={padding}>about</Link>
+      </div>
+      
+    </>
+  )
+}
+
+export const Footer = () => {
+  return(
+    <div>
+      <p>Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.</p>
+      <p>See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.</p>
+    </div>
+  )
+}
