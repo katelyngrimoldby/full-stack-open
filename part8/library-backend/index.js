f517169a3365eb9497c30cfe19b08e50e26d2a54
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-30 21:13:35 -0700

    8.3

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -92,22 +92,27 @@ const typeDefs = `#graphql
     name: String!
     born: Int
     id: ID!
+    bookCount: Int!
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
-
-
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+
+  Author: {
+    bookCount: (root) => books.filter(book => book.author === root.name).length
   }
 }
 
