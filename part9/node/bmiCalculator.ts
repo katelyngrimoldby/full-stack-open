fb8eed9427514adcfb30d9994e54775b1ff0fb7a
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-03 16:14:50 -0700

    9.5

--- part9/node/bmiCalculator.ts
+++ part9/node/bmiCalculator.ts
@@ -1,56 +1,80 @@
-const calculateBmi = (height: number, weight: number): string => {
+interface BmiResult {
+  height: number
+  weight: number
+  bmi: string
+}
+
+const calculateBmi = (height: number, weight: number): BmiResult => {
   const h = height / 100;
 
   const result = Math.round((weight / (h * h)) * 10) / 10
 
+  let bmi: string;
+
   switch(true) {
     case (result < 16): 
-      return 'Underweight (Severe thinness)'
+      bmi = 'Underweight (Severe thinness)'
+      break;
     case (result >= 16 && result <= 16.9): 
-      return 'Underweight (Moderate thinness)'
+      bmi = 'Underweight (Moderate thinness)'
+      break;
     case (result >= 17 && result <= 18.4):
-      return 'Underweight (Mild thinness)'
+      bmi = 'Underweight (Mild thinness)'
+      break;
     case (result >= 18.5 && result <= 24.9):
-      return 'Normal Range'
+      bmi = 'Normal Range'
+      break;
     case (result >= 25 && result <= 29.9):
-      return 'Overweight (Pre-obese)'
+      bmi = 'Overweight (Pre-obese)'
+      break;
     case (result >= 30 && result <= 34.9):
-      return 'Obese (Class I)'
+      bmi = 'Obese (Class I)'
+      break;
     case (result >= 35 && result <= 39.9):
-      return 'Obese (Class II)'
+      bmi = 'Obese (Class II)'
+      break;
     case (result >= 40):
-      return 'Obese (Class III)'
+      bmi = 'Obese (Class III)'
+      break;
     default: 
       throw new Error('What went wrong??')
   }
-}
-
-interface BmiValues {
-  height: number
-  weight: number
-}
-
-const parseBmiArgs = (args: string[]): BmiValues => {
-  if(args.length < 4) throw new Error('Missing arguments')
-  if(args.length > 4) throw new Error('Too many arguments')
-
-  const parameters = args.slice(2)
-  parameters.forEach(param => {
-    if(isNaN(Number(param))) {
-      throw new Error('One or more arguments are not numbers')
-    }
-  })
 
   return {
-    height: Number(args[2]),
-    weight: Number(args[3])
+    weight,
+    height,
+    bmi
   }
 }
-try {
-  const {height, weight} = parseBmiArgs(process.argv)
-  console.log(calculateBmi(height, weight))
-} catch(err) {
-  if(err instanceof Error) {
-    console.log(err.message)
-  }
-}
+
+// interface BmiValues {
+//   height: number
+//   weight: number
+// }
+
+// const parseBmiArgs = (args: string[]): BmiValues => {
+//   if(args.length < 4) throw new Error('Missing arguments')
+//   if(args.length > 4) throw new Error('Too many arguments')
+
+//   const parameters = args.slice(2)
+//   parameters.forEach(param => {
+//     if(isNaN(Number(param))) {
+//       throw new Error('One or more arguments are not numbers')
+//     }
+//   })
+
+//   return {
+//     height: Number(args[2]),
+//     weight: Number(args[3])
+//   }
+// }
+// try {
+//   const {height, weight} = parseBmiArgs(process.argv)
+//   console.log(calculateBmi(height, weight))
+// } catch(err) {
+//   if(err instanceof Error) {
+//     console.log(err.message)
+//   }
+// }
+
+export default calculateBmi
--- part9/node/index.ts
+++ part9/node/index.ts
@@ -1,10 +1,31 @@
 import express = require('express');
+import qs = require('qs');
+import calculateBmi from './bmiCalculator';
 
 const app = express()
 
+app.use('query parser', ((str: string) => qs.parse(str)))
+
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!')
-}) 
+})
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height)
+  const weight = Number(req.query.weight)
+
+  if(isNaN(height) || isNaN(weight)) res.status(400).json({error: 'Malformatted parameters'})
+
+  try {
+    const result = calculateBmi(height, weight)
+
+    res.json(result)
+  } catch(error) {
+    if(error instanceof Error) {
+      res.status(400).json({error: error.message})
+    }
+  }
+})
 
 const PORT = 3000
 
