0bdf7e13b6ccb9db09d772c255a7fadf92531ea1
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-03 15:51:04 -0700

    9.4

---
+++ part9/node/index.ts
@@ -0,0 +1,13 @@
+import express = require('express');
+
+const app = express()
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!')
+}) 
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
