9898a0d7bfb311dc25956902b2b067b30df62403
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-19 10:34:34 -0700

    13.16

--- part13/index.js
+++ part13/index.js
@@ -9,12 +9,14 @@ const {errorHandler} = require('./util/middleware')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const authorRouter = require('./controllers/authors')
 
 app.use(express.json())
 
-app.use('/api/blogs/', blogRouter)
+app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/authors', authorRouter)
 
 app.use(errorHandler)
 
