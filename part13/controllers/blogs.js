82506edeebdf43569e830bef0ef6e032085cb3f3
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-19 09:50:16 -0700

    13.14

--- part13/controllers/blogs.js
+++ part13/controllers/blogs.js
@@ -5,13 +5,26 @@ const {Blog, User} = require('../models')
 const { tokenExtractor } = require('../util/middleware')
 
 router.get('/', async (req, res) => {
-  const where = {}
+  let where = {}
 
   if (req.query.search) {
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`
+    where =  {
+      [Op.or]: [
+        {
+          title: {
+            [Op.iLike]: `%${req.query.search}%`
+          }
+        },
+        {
+          author: {
+            [Op.iLike]: `%${req.query.search}%`
+          }
+        }
+      ]
     }
+    console.log(where)
   }
+  
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
     include: {
