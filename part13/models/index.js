8bbcfa1a85039fd9d2258a6726b3b47dae2d3848
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-19 17:04:54 -0700

    13.19

--- part13/models/index.js
+++ part13/models/index.js
@@ -1,8 +1,14 @@
 const Blog = require("./blog");
+const ReadingList = require("./readingList");
 const User = require("./user");
+
+
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+
+User.belongsToMany(Blog, {through: ReadingList, as: 'readings'})
+Blog.belongsToMany(User, {through: ReadingList, as: 'saved_by'})
 
 module.exports = {
   Blog,
